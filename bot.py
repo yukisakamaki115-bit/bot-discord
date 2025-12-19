11bdda8bb4c3b1b87ea50a358e23650263f8ac84
yukisakamaki115 <yukisakamaki115@gmail.com>
2025-12-19 14:30:26 -0300

    Projeto inicial do bot (sem secrets)

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"🤖 Bot conectado como {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+bot.run(TOKEN)
